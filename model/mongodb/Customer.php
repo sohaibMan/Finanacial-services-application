9e9d3bd5af7de72d760eef03a4d38c1ca9ff7bab
Sohaib Manah <souhaibemanah@gmail.com>
2023-03-04 23:27:31 +0100

    enable mongodb only

--- model/mongodb/Customer.php
+++ model/mongodb/Customer.php
@@ -9,15 +9,25 @@ namespace MongoDB {
         }
         public function getCustomer($account_id)
         {
-            global $customers;
-            $account = $customers->findOne(['_id' => new \MongoDB\BSON\ObjectId($account_id)]);
-            return  $account;
+            try {
+
+                global $customers;
+                $account = $customers->findOne(['_id' => new \MongoDB\BSON\ObjectId($account_id)]);
+                return  ['status' => 'success', 'data' => $account];
+            } catch (\Exception $e) {
+                return  ['status' => 'failed', 'data' => $e->getMessage()];
+            }
         }
         public function  createCustomer($user_name, $name, $address, $email)
         {
-            global $customers;
-            $account = $customers->insertOne(['user_name' => $user_name, 'name' => $name, 'address' => $address, 'email' => $email, 'accounts' => []], []);
-            return ['_id' => $account->getInsertedId(), 'user_name' => $user_name, 'name' => $name, 'address' => $address, 'email' => $email, 'accounts' => []];
+            try {
+
+                global $customers;
+                $account = $customers->insertOne(['user_name' => $user_name, 'name' => $name, 'address' => $address, 'email' => $email, 'accounts' => []], []);
+                return ['status' => 'success', 'data' => ['_id' => $account->getInsertedId(), 'user_name' => $user_name, 'name' => $name, 'address' => $address, 'email' => $email, 'accounts' => []]];
+            } catch (\Exception $e) {
+                return  ['status' => 'failed', 'data' => $e->getMessage()];
+            }
         }
     }
 }
